cb52b6c5211ef933f3027e56958368f5ec5a6120
pefffpafff <tryakin_gd1@mail.ru>
2022-01-30 13:11:54 +0300

    add second/third windows

--- main.py
+++ main.py
@@ -1,18 +1,53 @@
 import sys
 
 from PyQt5 import QtWidgets
-from main_form import Ui_MainWindow
+import main_form
+import second_window
+import third_window
 
+
+#Создание класса основного окна
+class MainWindow(QtWidgets.QMainWindow, main_form.Ui_MainWindow):
+	def __init__(self):
+		super().__init__()
+		self.setupUi(self)
+		#self.twoWindow = None
+		self.openButton.clicked.connect(self.open_second_window)
+		self.secondOpenButton.clicked.connect(self.open_third_window)
+
+	#функция открытия второго окна
+	def open_second_window(self):
+		self.twoWindow = TwoWindow()
+		self.twoWindow.show()
+		self.close()
+
+	#функция открытия третьего окна
+	def open_third_window(self):
+		self.thirdwindow = ThirdWindow()
+		self.thirdwindow.show()
+		self.close()
+
+#Создание класса второго окна
+class TwoWindow(QtWidgets.QMainWindow, second_window.Ui_MainWindow):
+	def __init__(self):
+		super().__init__()
+		self.setupUi(self)
+
+#Создание класса третьего окна
+class ThirdWindow(QtWidgets.QMainWindow, third_window.Ui_MainWindow):
+	def __init__(self):
+		super().__init__()
+		self.setupUi(self)
+			
 
 if __name__ == '__main__':
-	#Create applications
+	#Создание приложения
 	app = QtWidgets.QApplication([sys.argv])
 
-	#Create form and init it
-	mainWindow = QtWidgets.QMainWindow()
-	ui = Ui_MainWindow()
-	ui.setupUi(mainWindow)
-	mainWindow.show()
+	#Запуск формы главного окна
+	window = MainWindow()
+	window.show()
+	
 
-	#Run main loop
+	#Запуск
 	sys.exit(app.exec())
