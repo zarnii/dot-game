e160d0434d0fb6c876ef2ba7822297f2d25bd2bf
pefffpafff <tryakin_gd1@mail.ru>
2022-01-30 12:20:14 +0300

    add main_form.ui/py

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-def main():
-	pass
+import sys
+
+from PyQt5 import QtWidgets
+from main_form import Ui_MainWindow
+
 
 if __name__ == '__main__':
-	main()
+	#Create applications
+	app = QtWidgets.QApplication([sys.argv])
+
+	#Create form and init it
+	mainWindow = QtWidgets.QMainWindow()
+	ui = Ui_MainWindow()
+	ui.setupUi(mainWindow)
+	mainWindow.show()
+
+	#Run main loop
+	sys.exit(app.exec())
