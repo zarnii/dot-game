221ee59cbe45333734541bab4906f05a015a7273
pefffpafff <tryakin_gd1@mail.ru>
2022-02-05 09:42:03 +0300

    add logics file

--- main.py
+++ main.py
@@ -1,43 +1,6 @@
 import sys
-
 from PyQt5 import QtWidgets
-import main_form
-import second_window
-import third_window
-
-
-#Создание класса основного окна
-class MainWindow(QtWidgets.QMainWindow, main_form.Ui_MainWindow):
-	def __init__(self):
-		super().__init__()
-		self.setupUi(self)
-		#self.twoWindow = None
-		self.openButton.clicked.connect(self.open_second_window)
-		self.secondOpenButton.clicked.connect(self.open_third_window)
-
-	#функция открытия второго окна
-	def open_second_window(self):
-		self.twoWindow = TwoWindow()
-		self.twoWindow.show()
-		self.close()
-
-	#функция открытия третьего окна
-	def open_third_window(self):
-		self.thirdwindow = ThirdWindow()
-		self.thirdwindow.show()
-		self.close()
-
-#Создание класса второго окна
-class TwoWindow(QtWidgets.QMainWindow, second_window.Ui_MainWindow):
-	def __init__(self):
-		super().__init__()
-		self.setupUi(self)
-
-#Создание класса третьего окна
-class ThirdWindow(QtWidgets.QMainWindow, third_window.Ui_MainWindow):
-	def __init__(self):
-		super().__init__()
-		self.setupUi(self)
+from logics import MainWindow
 			
 
 if __name__ == '__main__':
--- main_form.py
+++ main_form.py
@@ -15,6 +15,14 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(515, 538)
+        MainWindow.setStyleSheet("QPushButton{\n"
+"background-color: silver;\n"
+"border-radius:10;\n"
+"}\n"
+"\n"
+"QPushButton:hover{\n"
+"background-color:grey;\n"
+"}")
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.label = QtWidgets.QLabel(self.centralwidget)
@@ -24,22 +32,34 @@ class Ui_MainWindow(object):
         self.label.setFont(font)
         self.label.setObjectName("label")
         self.closeButton = QtWidgets.QPushButton(self.centralwidget)
-        self.closeButton.setGeometry(QtCore.QRect(170, 290, 191, 61))
+        self.closeButton.setGeometry(QtCore.QRect(20, 350, 171, 51))
         self.closeButton.setObjectName("closeButton")
         self.openButton = QtWidgets.QPushButton(self.centralwidget)
-        self.openButton.setGeometry(QtCore.QRect(200, 350, 131, 61))
+        self.openButton.setGeometry(QtCore.QRect(210, 350, 131, 51))
         self.openButton.setObjectName("openButton")
         self.secondOpenButton = QtWidgets.QPushButton(self.centralwidget)
-        self.secondOpenButton.setGeometry(QtCore.QRect(220, 410, 101, 61))
+        self.secondOpenButton.setGeometry(QtCore.QRect(360, 350, 141, 51))
         self.secondOpenButton.setObjectName("secondOpenButton")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 515, 21))
         self.menubar.setObjectName("menubar")
+        self.menu = QtWidgets.QMenu(self.menubar)
+        self.menu.setObjectName("menu")
+        self.menu_2 = QtWidgets.QMenu(self.menubar)
+        self.menu_2.setObjectName("menu_2")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
+        self.aSaveGame = QtWidgets.QAction(MainWindow)
+        self.aSaveGame.setObjectName("aSaveGame")
+        self.aLoadGame = QtWidgets.QAction(MainWindow)
+        self.aLoadGame.setObjectName("aLoadGame")
+        self.menu.addAction(self.aSaveGame)
+        self.menu.addAction(self.aLoadGame)
+        self.menubar.addAction(self.menu.menuAction())
+        self.menubar.addAction(self.menu_2.menuAction())
 
         self.retranslateUi(MainWindow)
         self.closeButton.clicked.connect(MainWindow.close)
@@ -52,3 +72,7 @@ class Ui_MainWindow(object):
         self.closeButton.setText(_translate("MainWindow", "close"))
         self.openButton.setText(_translate("MainWindow", "open"))
         self.secondOpenButton.setText(_translate("MainWindow", "open"))
+        self.menu.setTitle(_translate("MainWindow", "Файл"))
+        self.menu_2.setTitle(_translate("MainWindow", "Настройки"))
+        self.aSaveGame.setText(_translate("MainWindow", "Сохранить"))
+        self.aLoadGame.setText(_translate("MainWindow", "Загрузить"))
