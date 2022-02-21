f0985d6e4265e217ca7e2a5ca09df60e94370faf
pefffpafff <tryakin_gd1@mail.ru>
2022-02-21 14:44:07 +0300

    Add playground and dot drawing function

--- logics.py
+++ logics.py
@@ -1,5 +1,9 @@
 from PyQt5 import QtWidgets
-from PyQt5.QtCore import QTimer
+#from PyQt5.QtCore import QTimer
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+
 import main_form
 import second_window
 import third_window
@@ -9,13 +13,48 @@ class MainWindow(QtWidgets.QMainWindow, main_form.Ui_MainWindow):
 	def __init__(self):
 		super().__init__()
 		self.setupUi(self)
+		self.points = []
 		self.moveTimer = QTimer(self)
 		self.openButton.clicked.connect(self.open_second_window)
 		self.secondOpenButton.clicked.connect(self.open_third_window)
 		self.aLoadGame.triggered.connect(self.open_second_window)
 		self.playButton.clicked.connect(self.play)
 		self.stopButton.clicked.connect(self.stop)
-		self.moveTimer.timeout.connect(self.onMoveTimerTimeout)
+		#self.moveTimer.timeout.connect(self.onMoveTimerTimeout)
+
+	def mousePressEvent(self, e):
+		print("Mouse pressed at", e.x(), e.y())
+		if (e.x() % 20 == 0) and (e.y() % 20 == 0):
+			self.points.append([e.x(), e.y()])
+		self.update() # вся перерисовка только через update,
+					  # который сам вызовет paintEvent.
+
+	'''_________Create playground_________'''   
+	def paintEvent(self, event):
+		p = QPainter()
+		p.begin(self)
+		pen = QPen(Qt.black, 1)
+		p.setPen(pen)
+
+		
+		x = 20
+		while x <= 520:
+			p.drawLine(x,100,x,500)
+			x += 20
+
+		y = 100
+		while y < 520:
+			p.drawLine(20,y,500,y)
+			y += 20
+		#painter.drawLine(10,200,510,200)
+		
+		pen = QPen(Qt.red, 5)
+		p.setPen(pen)
+		for x, y in self.points:
+			p.drawPoint(x, y)
+
+		p.end()
+
 
 	#функция открытия второго окна
 	def open_second_window(self):
@@ -35,10 +74,10 @@ class MainWindow(QtWidgets.QMainWindow, main_form.Ui_MainWindow):
 	def stop(self):
 		self.moveTimer.stop()
 
-	def onMoveTimerTimeout(self):
-		#self.label.setGeometry(100,100,100,100)
-		geom = self.label.geometry()
-		self.label.setGeometry(geom.x(), (geom.y()+10) % self.centralwidget.height(), geom.width(), geom.height())
+	'''def onMoveTimerTimeout(self):
+					#self.label.setGeometry(100,100,100,100)
+					geom = self.label.geometry()
+					self.label.setGeometry(geom.x(), (geom.y()+10) % self.centralwidget.height(), geom.width(), geom.height())'''
 
 
 #Создание класса второго окна
