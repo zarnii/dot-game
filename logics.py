6656e5caf4c814e7a63a2f601d7c81a71f603918
pefffpafff <tryakin_gd1@mail.ru>
2022-03-20 18:57:50 +0300

    jopa polnaya

--- logics.py
+++ logics.py
@@ -5,8 +5,6 @@ from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 
 import main_form
-import second_window
-import third_window
 import menu
 
 
@@ -19,7 +17,7 @@ class MainWindow(QtWidgets.QMainWindow, main_form.Ui_MainWindow):
 		self.secondPlayerPoints = []
 		self.player = 1
 
-		self.mainWindowButton.clicked.connect(self.openMenuWindow)
+		self.mainWindowButton.clicked.connect(lambda:self.openMenuWindow())
 		#self.moveTimer = QTimer(self)
 		#self.openButton.clicked.connect(self.open_second_window)
 		#self.secondOpenButton.clicked.connect(self.open_third_window)
@@ -28,43 +26,79 @@ class MainWindow(QtWidgets.QMainWindow, main_form.Ui_MainWindow):
 		#self.stopButton.clicked.connect(self.stop)
 		#self.moveTimer.timeout.connect(self.onMoveTimerTimeout)
 
+	#Возвращение в главное меню из игрового меню
 	def openMenuWindow(self):
 		self.menuWindow = MenuWindow()
 		self.menuWindow.show()
 		self.close()
 
+	#Выводит на экран ход игрока
 	def playerTurn(self):
 		if self.player % 2 == 0:
 			self.Player_turn.setText('Ходит игрок 2')
 		elif self.player % 2 != 0:
 			self.Player_turn.setText('Ходит игрок 1')
 
-
+	'''Здесь полная жопа, надо потом сделать все по красоте
+	   Идет отслежование нажатий мышки на экран 
+	   и эти координаты добовляюся в списки	
+	'''
 	def mousePressEvent(self, e):
 		'''_____Round cords______'''
 		x = round(e.x()/10)*10
 		y = round(e.y()/10)*10
 		print("Mouse pressed at", x, y)
 
-		print("Mouse pressed at (round)", )
+		x_y_in_firstPlayerPoints = False
+		x_y_in_secondPlayerPoints = False
+
 		if (x % 20 == 0) and (y % 20 == 0):
 			if self.player % 2 == 0:
 				self.player += 1
-				self.Player_turn.setText('Ходит игрок 2')
-				self.secondPlayerPoints.append([x, y])
-				print('it was a second player')
-				print(self.player)
-				print(self.secondPlayerPoints)
-				self.playerTurn()
+
+				#Идет проверка на наличе на этом месте точки 
+				for i in self.firstPlayerPoints:
+					if x in i and y in i:
+						x_y_in_firstPlayerPoints = True
+				for i in self.secondPlayerPoints:
+					if x in i and y in i:
+						x_y_in_secondPlayerPoints = True
+				print('нажал второй игрок')
+
+				#Если на этом месте нет точки, то создать ее там 
+				if x_y_in_firstPlayerPoints == False and x_y_in_secondPlayerPoints == False:
+					self.secondPlayerPoints.append([x, y])
+					print('\nЭто место свободно!')
+					print('Координаты добавились')
+					#print(self.player)
+					print(self.secondPlayerPoints)
+					self.playerTurn()
+				else:
+					print('Это место уже занято!')
+
 				
 			elif self.player % 2 != 0:
 				self.player += 1
-				self.firstPlayerPoints.append([x, y])
-				print('it was a first player')
-				print(self.player)
-				print(self.firstPlayerPoints)
-				self.playerTurn()
 
+				#Идет проверка на наличе на этом месте точки 
+				for i in self.firstPlayerPoints:
+					if x in i and y in i:
+						x_y_in_firstPlayerPoints = True
+				for i in self.secondPlayerPoints:
+					if x in i and y in i:
+						x_y_in_secondPlayerPoints = True
+				print('нажал первый игрок')
+
+				#Если на этом месте нет точки, то создать ее там 
+				if x_y_in_firstPlayerPoints == False and x_y_in_secondPlayerPoints == False:
+					print('\nЭто место свободно!')
+					self.firstPlayerPoints.append([x, y])
+					print('Координаты добавились')
+					#print(self.player)
+					print(self.firstPlayerPoints)
+					self.playerTurn()
+				else:
+					print('Это место уже занято!')
 		self.update() 
 
 	'''_________Create playground_________'''   
@@ -105,20 +139,16 @@ class MainWindow(QtWidgets.QMainWindow, main_form.Ui_MainWindow):
 			p.drawPoint(x, y)
 		p.end()
 
-		for point in self.firstPlayerPoints:
-			pass
-			
-
-	def open_second_window(self):
-		self.twoWindow = TwoWindow()
-		self.twoWindow.show()
-		self.close()
-
-
-	def open_third_window(self):
-		self.thirdwindow = ThirdWindow()
-		self.thirdwindow.show()
-		self.close()
+	'''def keyPressEvent(self, e):
+					if e.key() == Qt.Key_A:
+						print('Left')
+					elif e.key() == Qt.Key_D:
+						print('Right')
+					elif e.key() == Qt.Key_W:
+						print('Up')
+					elif e.key() == Qt.Key_S:
+						print('Down')
+			'''
 
 	def play(self):
 		self.moveTimer.start(100)
@@ -131,6 +161,7 @@ class MainWindow(QtWidgets.QMainWindow, main_form.Ui_MainWindow):
 					geom = self.label.geometry()
 					self.label.setGeometry(geom.x(), (geom.y()+10) % self.centralwidget.height(), geom.width(), geom.height())'''
 
+#Главное меню всего приложения
 class MenuWindow(QtWidgets.QMainWindow, menu.Ui_MainWindow):
 	"""docstring for MenuWindow"""
 	def __init__(self):
@@ -144,17 +175,5 @@ class MenuWindow(QtWidgets.QMainWindow, menu.Ui_MainWindow):
 		self.mainWindow.show()
 		self.close()
 
-#Создание класса второго окна
-class TwoWindow(QtWidgets.QMainWindow, second_window.Ui_MainWindow):
-	def __init__(self):
-		super().__init__()
-		self.setupUi(self)
-
-
-#Создание класса третьего окна
-class ThirdWindow(QtWidgets.QMainWindow, third_window.Ui_MainWindow):
-	def __init__(self):
-		super().__init__()
-		self.setupUi(self)
 
 			
